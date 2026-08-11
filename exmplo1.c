fb9cf159e7d4bce6fa737924de8eecf2be7771e3
Jonathan50x <ja785071@gmail.com>
2026-08-11 23:41:03 +0000

    meu exemplo

--- exmplo1.c
+++ exmplo1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+	printf("Programa simples em C: soma de dois inteiros\n");
+	printf("Digite dois inteiros separados por espaço: ");
+	if (scanf("%d %d", &a, &b) != 2) {
+		printf("Entrada inválida.\n");
+		return 1;
+	}
+	printf("Soma: %d\n", a + b);
+	return 0;
+}
 
